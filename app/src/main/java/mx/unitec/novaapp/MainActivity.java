98de6a546a9cfc95c64ef79273698c80fe1ee9c7
KevSaldivar <kevinsldv@hotmail.com>
2025-04-21 15:18:50 -0600

    :boom: Creating Classes Home, SignIn, SignUp, Main

--- app/src/main/java/mx/unitec/novaapp/MainActivity.java
+++ app/src/main/java/mx/unitec/novaapp/MainActivity.java
@@ -1,4 +1,24 @@
 package mx.unitec.novaapp;
 
-public class MainActivity {
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SharedPreferences prefs = getSharedPreferences("auth", MODE_PRIVATE);
+        boolean isLoggedIn = prefs.getBoolean("isLoggedIn", false);
+
+        if (isLoggedIn) {
+            startActivity(new Intent(this, HomeActivity.class));
+        } else {
+            startActivity(new Intent(this, SignInActivity.class));
+        }
+
+        finish();
+    }
 }
